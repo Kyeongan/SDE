1bd54ff0791022a1da0df070dfc9d3d6239114c9
Karl <newmkka@gmail.com>
2015-07-06 19:33:20 -0500

    Updates

--- SDE/src/edu/uh/cs/sde/Node.java
+++ SDE/src/edu/uh/cs/sde/Node.java
@@ -1,8 +1,21 @@
 package edu.uh.cs.sde;
 
 public class Node {
+	
+	int value;
 	Node left;
 	Node right;
-	int value;
+	
+	Node(){
+		System.out.println("Node Class");
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
 
 }
