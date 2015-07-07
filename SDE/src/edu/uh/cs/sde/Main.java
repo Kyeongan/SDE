72c792ba05f7e8be1cfdcbd693dd3534f4457b81
Karl <newmkka@gmail.com>
2015-07-06 19:29:26 -0500

    First checkin

---
+++ SDE/src/edu/uh/cs/sde/Main.java
@@ -0,0 +1,16 @@
+package edu.uh.cs.sde;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		displayGreeting();
+
+	}
+
+	private static void displayGreeting() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello!");
+	}
+
+}
