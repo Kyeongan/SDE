bc341b3dbe068d99ea8c1cbaa33cc8bc5aa3aad0
Karl <newmkka@gmail.com>
2015-07-06 19:51:44 -0500

    Need more getter/setter methods

--- SDE/src/edu/uh/cs/sde/Node.java
+++ SDE/src/edu/uh/cs/sde/Node.java
@@ -18,4 +18,20 @@ public class Node {
 		this.value = value;
 	}
 
+	public Node getLeft() {
+		return left;
+	}
+
+	public void setLeft(Node left) {
+		this.left = left;
+	}
+
+	public Node getRight() {
+		return right;
+	}
+
+	public void setRight(Node right) {
+		this.right = right;
+	}
+
 }
