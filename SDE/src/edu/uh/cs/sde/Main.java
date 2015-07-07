ede10e530b925dea0f01d0269660b7e023922476
Karl <newmkka@gmail.com>
2015-07-06 19:37:13 -0500

    Cleaning2

--- SDE/src/edu/uh/cs/sde/Main.java
+++ SDE/src/edu/uh/cs/sde/Main.java
@@ -3,7 +3,7 @@ package edu.uh.cs.sde;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		displayGreeting();
 		Node myNode = new Node();
 
