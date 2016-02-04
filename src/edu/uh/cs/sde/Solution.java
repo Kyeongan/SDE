9c90ad662a0238f7823e8f899be538b706dc9715
Kyeongan Kwon <newmkka@gmail.com>
2016-02-03 21:53:33 -0600

    Added Try Catch statment

---
+++ src/edu/uh/cs/sde/Solution.java
@@ -0,0 +1,28 @@
+package edu.uh.cs.sde;
+
+import java.util.*;
+import java.util.Scanner;
+
+class Solution{
+
+    public static void main(String []argh)
+    {
+        Scanner in = new Scanner(System.in);
+
+        while(in.hasNextInt())
+        {
+            int n = in.nextInt();
+            int p = in.nextInt();
+            MyCalculator M = new MyCalculator();
+            try
+            {
+                System.out.println(M.power(n,p));
+            }
+            catch(Exception e)
+            {
+                System.out.println(e);
+            }
+        }
+
+    }
+}
--- src/edu/uh/cs/sde/bst/Node.java
+++ src/edu/uh/cs/sde/bst/Node.java
@@ -6,6 +6,14 @@ public class Node {
 	Node right;
 	
 	static Node lca(Node root, int v1, int v2) {
-		
+		if (root != null ) {
+			if (root.data > v1 && root.data > v2 ) 
+				root = root.left;
+			else if (root.data < v1 && root.data < v2 )
+				root = root.right;
+			else 
+				return root;			
+		}		
+		return null;
 	}
 }
