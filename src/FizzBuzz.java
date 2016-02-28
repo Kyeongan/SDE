199d9d330b1301139487b63f3bf99d2b7a0a41a7
Kyeongan Kwon <newmkka@gmail.com>
2016-02-27 22:39:13 -0600

    Fizz Buzz

---
+++ src/FizzBuzz.java
@@ -0,0 +1,22 @@
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i <= 100; i++) {
+			boolean fizz = (i % 3) == 0;
+			boolean buzz = (i % 5) == 0;
+
+			if (fizz && buzz) {
+				System.out.println("fizzbuzz");
+			} else if (fizz) {
+				System.out.println("fizz");
+			} else if (buzz) {
+				System.out.println("buzz");
+			} else {
+				System.out.println(i);
+			}
+
+		}
+	}
+}
