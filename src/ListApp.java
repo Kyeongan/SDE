fd7ec5b12058c2d1d93d4a17ecfde9d1e17d55f5
Kyeongan Kwon <newmkka@gmail.com>
2016-03-12 16:45:29 -0600

    Difference between ArrayList and LinkedList in Java collections

---
+++ src/ListApp.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class ListApp {
+
+	public static void main(String[] args) {
+		// ArrayList manages arrays
+		List<Integer> arrayList = new ArrayList<Integer>();
+		
+		// LinkedList is a double linked list which has a reference to previous and next elements
+		List<Integer> linkedList = new LinkedList<Integer>();
+		
+		doTiming("ArrayList", arrayList);
+		doTiming("LinkedList", linkedList);
+
+	}
+
+	private static void doTiming(String text, List<Integer> list) {
+		
+		long startTime = System.currentTimeMillis();
+		
+		// Populate
+		for(int i = 0; i<1E5 ; i++) {
+			list.add(i);
+		}
+		
+		
+		for(int i = 0; i<1E5 ; i++) {
+			list.add(0, i);
+		}
+		
+		
+		long endTime = System.currentTimeMillis();
+		
+		System.out.println("Time taken: " + (endTime-startTime) + " ms for " + text);
+		
+	}
+
+}
