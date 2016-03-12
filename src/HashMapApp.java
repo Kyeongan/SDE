0187cadcfdb0a8c02e505dcd4ee72cb652c3f205
Kyeongan Kwon <newmkka@gmail.com>
2016-03-12 17:02:20 -0600

    Entry and EntrySet in HaspMap

---
+++ src/HashMapApp.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class HashMapApp {
+
+	public static void main(String argc[]) {
+		HashMap<Integer, String> map = new HashMap<Integer, String>();
+		
+		map.put(0, "Zero");
+		map.put(1, "One");
+		map.put(2, "Two");
+		map.put(3, "Three");
+		
+		System.out.println(map.get(1));
+		
+		for(int i=0; i<map.size() ; i++) {
+			System.out.println(map.get(i));
+		}
+
+		for(Entry<Integer, String> entry : map.entrySet() ) {
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+		}
+	}
+}
