7f8936bb9d29a91b02ccf219a2ddf8b0ea58802e
Kyeongan Kwon <newmkka@gmail.com>
2016-03-12 17:36:30 -0600

    HashMap, LinkedHashMap, TreeMap. which map guarantee the order

---
+++ src/MapApp.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MapApp {
+
+	public static void main(String[] args) {
+		Map<Integer, String> hashMap = new HashMap<Integer, String>();
+		Map<Integer, String> linkedHashMap = new LinkedHashMap<Integer, String>();
+		Map<Integer, String> treeMap = new TreeMap<Integer, String>();
+		
+		testMap(hashMap);
+		testMap(linkedHashMap);
+		testMap(treeMap);
+		
+
+	}
+	
+	private static void testMap(Map<Integer, String> map ) {
+		map.put(9, "Nine");
+		map.put(0, "Zero");
+		map.put(5, "Five");
+		map.put(3, "Three");
+		map.put(2, "Two");
+		map.put(7, "Seven");
+		
+		for (Integer key : map.keySet()) {
+			String value = map.get(key);
+			System.out.println(key + " : " + value);
+		}
+		System.out.println();
+	}
+
+}
