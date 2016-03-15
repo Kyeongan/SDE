aef4701460e84574f16d8591a21bda7519b02c41
Kyeongan Kwon <newmkka@gmail.com>
2016-03-14 22:33:25 -0500

    PhoneBook with HashMap

---
+++ src/PhoneBook.java
@@ -0,0 +1,29 @@
+
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+
+public class PhoneBook {
+	public static void main(String[] argh) {
+		Scanner in = new Scanner(System.in);
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		int n = in.nextInt();
+		in.nextLine();
+		for (int i = 0; i < n; i++) {
+			String name = in.nextLine();
+			int phone = in.nextInt();
+			map.put(name, phone);
+			in.nextLine();
+		}
+
+		while (in.hasNext()) {
+			String name = in.nextLine();
+			Integer phonenumber = map.get(name);
+			if (!map.containsKey(name)) {
+				System.out.println("Not found");
+			}
+			else 
+				System.out.println("Name:" + name + " - " + phonenumber);
+		}
+	}
+}
