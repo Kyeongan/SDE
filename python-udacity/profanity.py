71c42e127639c567c2edc916a879122ccae158c5
Kyeongan Kwon <newmkka@gmail.com>
2016-07-04 13:36:53 -0500

    renaming

--- python-udacity/profanity.py
+++ python-udacity/profanity.py
@@ -1,1 +1,7 @@
-profanity
+def read_text():
+    quotes = open("/Users/karlkyeongankwon/git/SDE/python-udacity/movie_quotes.txt")
+    contents_of_file = quotes.read()
+    print(contents_of_file)
+    quotes.close()
+
+read_text()
