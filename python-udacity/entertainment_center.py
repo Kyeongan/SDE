9fb3da59244205076ec35c6d525fced3f9d0763e
Kyeongan Kwon <newmkka@gmail.com>
2016-07-05 12:30:40 -0500

    replaced story line to year for sorting

--- python-udacity/entertainment_center.py
+++ python-udacity/entertainment_center.py
@@ -2,20 +2,27 @@ import fresh_tomatoes
 import media
 
 terminator_2 = media.Movie("Terminator 2",
-                        "Terminator 2: Judgment Day",
+                        "1991",
                         "http://t1.gstatic.com/images?q=tbn:ANd9GcS5J6Ay6y1UT7WAI4U7Zm2KDYITrvfOI3vmaCNdGhx_0jmWiI1d",
                         "https://www.youtube.com/watch?v=eajuMYNYtuY")
 
 matrix = media.Movie("The Matrix",
-                    "The Matrix",
+                    "1999",
                     "http://ia.media-imdb.com/images/M/MV5BMTkxNDYxOTA4M15BMl5BanBnXkFtZTgwNTk0NzQxMTE@._V1_UX182_CR0,0,182,268_AL_.jpg",
                     "https://www.youtube.com/watch?v=oZ1-M8O70zk")
 
 inception = media.Movie("Inception",
-                    "Inception",
+                    "2010",
                     "http://t2.gstatic.com/images?q=tbn:ANd9GcRo9vfJCM6dzPkZHIHBVCtlJnAnew9Ai26kEdrli0-tfmatmciD",
                     "https://www.youtube.com/watch?v=66TuSJo4dZM")
 
-movies = [terminator_2, matrix, inception]
+wolf_of_wall_street = media.Movie("The Wolf of Wall Street",
+                    "2013",
+                    "http://t1.gstatic.com/images?q=tbn:ANd9GcTm52BWbxXmwOpfz5rmx0BNBj79kSQoGpYPq4TVt07Jel5En6aC",
+                    "https://www.youtube.com/watch?v=iszwuX1AK6A")
+
+
+movies = [terminator_2, matrix, inception, wolf_of_wall_street]
+
 fresh_tomatoes.open_movies_page(movies)
-print(media.Movie.VALID_RATINGS)
+# print(media.Movie.VALID_RATINGS)
--- python-udacity/media.py
+++ python-udacity/media.py
@@ -3,9 +3,9 @@ import webbrowser
 class Movie():
     """ This class provides a way to store movie related information """
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, year, poster_image, trailer_youtube):
         self.title = movie_title
-        self.storyline = movie_storyline
+        self.year = year
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
