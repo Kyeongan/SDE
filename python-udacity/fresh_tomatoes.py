817e7635f86b482ee700bdd8dc834ee75ceee7a6
Kyeongan Kwon <newmkka@gmail.com>
2016-07-05 12:50:21 -0500

    updated

--- python-udacity/fresh_tomatoes.py
+++ python-udacity/fresh_tomatoes.py
@@ -107,7 +107,7 @@ main_page_content = '''
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Karl's Favorites Movies by Fresh Tomatoes Movie Trailers</a>
+            <a class="navbar-brand" href="#">Karl's Favorites Movies Website created by Python</a>
           </div>
         </div>
       </div>
