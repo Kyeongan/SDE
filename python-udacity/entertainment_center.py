16f24e7fc7a2c79c0517ad62c0920793a0468733
Kyeongan Kwon <newmkka@gmail.com>
2016-07-15 18:43:43 -0500

    grad system

--- python-udacity/entertainment_center.py
+++ python-udacity/entertainment_center.py
@@ -27,15 +27,15 @@ movies.append(media.Movie("Shutter Island",
     "http://www.gstatic.com/tv/thumb/movieposters/3531967/p3531967_p_v8_ah.jpg",
     "https://www.youtube.com/watch?v=5iaYLCiq5RM"))
 
-movies.append(media.Movie("Terminator 2",
-    "1991",
-    "http://t1.gstatic.com/images?q=tbn:ANd9GcS5J6Ay6y1UT7WAI4U7Zm2KDYITrvfOI3vmaCNdGhx_0jmWiI1d",
-    "https://www.youtube.com/watch?v=eajuMYNYtuY"))
-
 movies.append(media.Movie("The Martian",
     "2015",
     "http://t2.gstatic.com/images?q=tbn:ANd9GcTkKPZ7EIOafEsemyn6zTIDeGYthKC_Okgxi1eX6diuOT3xKWXQ",
     "https://www.youtube.com/watch?v=ej3ioOneTy8"))
+
+movies.append(media.Movie("Gravity",
+    "2013",
+    "http://t1.gstatic.com/images?q=tbn:ANd9GcRu6dxMqi3cxDsmH1ajpW3Kr-bKPh2Nf57GXRFW4ybrEgcaUu-C",
+    "https://www.youtube.com/watch?v=OiTiKOy59o4"))
 
 
 movies_sorted = sorted(movies, key=lambda movies: movies.year, reverse=True)
