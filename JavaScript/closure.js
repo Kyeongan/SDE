69517ad0e31d6d1837800e329ce6399146019b2b
Karl Kwon <newmkka@gmail.com>
2017-01-30 22:41:37 -0600

    add self-invoke function

--- JavaScript/closure.js
+++ JavaScript/closure.js
@@ -1,6 +1,19 @@
+// Jan 30, 2017
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
+
 var me = "Karl Kwon";
 function hello() {
 	console.log(me);
 }
 
 hello()
+
+
+function init() {
+    var name = "Mozilla"; // name is a local variable created by init
+    function displayName() { // displayName() is the inner function, a closure
+        console.log (name); // displayName() uses variable declared in the parent function    
+    }
+    displayName();    
+}
+init();
