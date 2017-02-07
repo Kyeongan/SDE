b242e04f50ed6235b32441ac85f39686becd8c20
Karl Kwon <newmkka@gmail.com>
2017-02-06 18:31:21 -0600

    Fixed

--- JavaScript/Apple.js
+++ JavaScript/Apple.js
@@ -113,21 +113,21 @@ for( var i=0 ; i<date.length ; i++ ) {
   // month
   switch(date[i].substring(mm_index, mm_index+3))
   {
-   case 'Jan' :mm = 1;
-   case 'Feb' :mm = 2;
-   case 'Mar' :mm = 3;
-   case 'Apr' :mm = 4;
-   case 'May' :mm = 5;
-   case 'Jun' :mm = 6;
-   case 'Jul' :mm = 7;
-   case 'Aug' :mm = 8;
-   case 'Sep' :mm = 9;
-   case 'Oct' :mm = 10;
-   case 'Nov' :mm = 11;
-   case 'Dec' :mm = 12;
+   case 'Jan' :mm = '01'; break;
+   case 'Feb' :mm = '02'; break;
+   case 'Mar' :mm = '03'; break;
+   case 'Apr' :mm = '04'; break;
+   case 'May' :mm = '05'; break;
+   case 'Jun' :mm = '06'; break;
+   case 'Jul' :mm = '07'; break;
+   case 'Aug' :mm = '08'; break;
+   case 'Sep' :mm = '09'; break;
+   case 'Oct' :mm = '10'; break;
+   case 'Nov' :mm = '11'; break;
+   case 'Dec' :mm = '12'; break;
   }
   
-  // console.log(date[i].substring(date[i].length - 2));
+  // console.log(date[i].substring(date[i].length - 2)) break;
   
   if( date[i].substring(date[i].length - 2) < 9999
         || date[i].substring(date[i].length - 2) > 0 )
@@ -139,5 +139,4 @@ for( var i=0 ; i<date.length ; i++ ) {
 }
 
 console.log(output);
-
 
